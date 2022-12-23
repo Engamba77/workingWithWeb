bae5e213d9fcb4e963e9b2ddb162f620d9367e81
Engamba <engambasingha@gmail.com>
2022-12-23 13:42:06 +0530

    Adding changes to timezone

--- timeZones/index.js
+++ timeZones/index.js
@@ -1,13 +1,38 @@
-function displayTime() {
-  const date = new Date();
-  const indiaTimeZone = date.toLocaleString("en-US", {
-    timeZone: "Asia/Kolkata",
-  });
-  //   const tokyoTimeZone = date.toLocaleString("en-US", {
-  //     timeZone: "Asia/Kolkata",
-  //   });
+const timezone1 = document.getElementById("timezone1");
+const timezone2 = document.getElementById("timezone2");
+const timezone3 = document.getElementById("timezone3");
+const btn = document.getElementById("btn");
 
-  document.getElementById("time").innerHTML = indiaTimeZone;
-  //   document.getElementById("time").innerHTML = tokyoTimeZone;
+let isPlaying = false;
+let clockHandler;
+
+function displayTime() {
+  const time1 = new Date().toLocaleTimeString("en-US", {
+    timeZone: "America/New_York",
+  });
+  const time2 = new Date().toLocaleTimeString("en-US", {
+    timeZone: "Europe/Paris",
+  });
+  const time3 = new Date().toLocaleTimeString("ar-EG", {
+    timeZone: "Asia/Tokyo",
+  });
+
+  timezone1.innerHTML = time1;
+  timezone2.innerHTML = time2;
+  timezone3.innerHTML = time3;
 }
-setInterval(displayTime, 0);
+
+btn.addEventListener("click", playClock);
+
+function playClock() {
+  if (isPlaying) {
+    clearInterval(clockHandler);
+    isPlaying = false;
+    btn.innerHTML = "Play";
+    return;
+  }
+  isPlaying = true;
+  btn.innerHTML = "Pause";
+  clockHandler = setInterval(displayTime, 1000);
+}
+playClock();
